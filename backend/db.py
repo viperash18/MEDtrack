5e1ecadd6ef259a2ff8ccfd753bac05e52391691
Ashwary rathore <ashwaryrathore209@gmail.com>
2026-07-22 18:04:11 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -3,10 +3,10 @@ from flask import g, current_app
 
 
 def get_db():
-    if "db" not in g:
-        g.db = sqlite3.connect(current_app.config["DATABASE"])
+    if "db" not in g: #if the g object is empty so create new g obect
+        g.db = sqlite3.connect(current_app.config["DATABASE"])#this is actually connecting the g object with the database 
         g.db.row_factory = sqlite3.Row
-        g.db.execute("PRAGMA foreign_keys = ON")
+        g.db.execute("PRAGMA foreign_keys = ON")#SQlite by default disables foriegn keys s to turn it on
     return g.db
 
 
